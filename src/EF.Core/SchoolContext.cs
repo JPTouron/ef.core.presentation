3a75b2a318f29eab9b6ab6795c5da3865e26873b
JP <jp.touron@gmail.com>
2018-02-03 18:25:48 -0300

    added dbcontext

---
+++ src/EF.Core/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using EF.Core.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.Core
+{
+    public class SchoolContext : DbContext
+    {
+        protected SchoolContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = School; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+            optionsBuilder.UseSqlServer(connString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
